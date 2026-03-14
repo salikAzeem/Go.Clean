cac94ab16e8b0e557be4372d976076223c3c7f4e
salik azeem <salikazeem123@gmail.com>
2026-03-14 20:08:16 +0530

    phone view

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -2,6 +2,7 @@ import { Home, Camera, FileText } from "lucide-react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
+
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -9,7 +10,9 @@ const BottomBar = () => {
     location.pathname === path ? "text-green-400" : "text-white";
 
   return (
+
     <div className="fixed bottom-0 left-0 w-full bg-[#1f3b57] shadow-2xl md:hidden z-50">
+
       <div className="flex justify-around items-center h-16 relative">
 
         {/* HOME */}
@@ -20,13 +23,15 @@ const BottomBar = () => {
           <Home size={24} />
         </button>
 
+
         {/* CENTER CAMERA */}
         <button
-          onClick={() => navigate("/camera")}
+          onClick={() => navigate("/scan")}
           className="absolute -top-6 bg-green-500 p-4 rounded-full shadow-xl border-4 border-[#1f3b57]"
         >
           <Camera size={26} className="text-white" />
         </button>
+
 
         {/* REPORT */}
         <button
@@ -37,8 +42,11 @@ const BottomBar = () => {
         </button>
 
       </div>
+
     </div>
+
   );
+
 };
 
 export default BottomBar;
