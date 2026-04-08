f7a4b5ded3d229875dc40eda7bed260421f697e0
salik azeem <salikazeem123@gmail.com>
2026-04-08 16:52:18 +0530

    background

--- src/components/GlobalHero.tsx
+++ src/components/GlobalHero.tsx
@@ -23,10 +23,10 @@ const images = [
   //sar,
   few,
   hhh,
-  abc,
   //five,
   six,
   seven,
+  abc,
   eight,
   nine
 ];
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,10 +27,10 @@ const images = [
   //sar,
   few,
   hhh,
-  abc,
   //five,
   six,
   seven,
+  abc,
   eight,
   nine
 ];
